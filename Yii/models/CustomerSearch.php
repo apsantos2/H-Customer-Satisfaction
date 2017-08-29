cabf4daf81b71ee2f316905fccb7f68b89147ae7
Bea <ramosmelissa696@gmail.com>
2017-08-29 14:09:04 +0800

    customer search model

---
+++ Yii/models/CustomerSearch.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Customer;
+
+/**
+ * CustomerSearch represents the model behind the search form about `app\models\Customer`.
+ */
+class CustomerSearch extends Customer
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id'], 'integer'],
+            [['cust_number', 'cust_name', 'cust_email', 'cust_type'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Customer::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+        ]);
+
+        $query->andFilterWhere(['like', 'cust_number', $this->cust_number])
+            ->andFilterWhere(['like', 'cust_name', $this->cust_name])
+            ->andFilterWhere(['like', 'cust_email', $this->cust_email])
+            ->andFilterWhere(['like', 'cust_type', $this->cust_type]);
+
+        return $dataProvider;
+    }
+}
