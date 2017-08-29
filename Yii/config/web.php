730afe095af284daba581120810780455965526c
Albern Santos <albernsantos18@gmail.com>
2017-08-29 12:13:48 +0800

    final update in web config

--- Yii/config/web.php
+++ Yii/config/web.php
@@ -60,4 +60,12 @@ if (YII_ENV_DEV) {
     ];
 }
 
+$config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
 return $config;
