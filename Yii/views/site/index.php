89711fd715d6f3717d945f05d23a593f11788602
Albern Santos <albernsantos18@gmail.com>
2017-08-29 13:23:54 +0800

    UPDATED UPDATED SITE FOR CUSTOMIZATION

--- Yii/views/site/index.php
+++ Yii/views/site/index.php
@@ -21,3 +21,35 @@ $this->title = 'Taal Vista Hotel';
     <img src="https://s2.postimg.org/5k8esf52x/taal.jpg" style="width:1000px;height:500px;"> 
     
   </div>
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  <div class="body-content">
+
+
+            <div class="paneSection">
+            <div class="about">
+                <h1 style="text-align:center;">Customer Satisfaction Monitoring System </h1>
+
+                    <p style="text-indent:50px, text-align: justify; ">Customer feedback is a marketing term that describes the process of obtaining a customer’s opinion
+					about a business, product or service. Customer Satisfaction Monitoring System will gather customer satisfaction feedback from Taal Vista Hotel guests. 
+					One of the goals of the proposed system is to analyze customer satisfaction feedback. 
+					Using rating-based questions when measuring customer satisfaction will help track and monitor
+					how satisfied customers are over time especially with frequent guests.</p>
+					
+      
+    </div>
+ </div>
