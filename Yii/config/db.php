219a889dfde1bcc3388f3aed2ea60aab07cfb2d4
Albern Santos <albernsantos18@gmail.com>
2017-08-29 11:46:02 +0800

    updated db name

--- Yii/config/db.php
+++ Yii/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=guestform',
+    'dsn' => 'mysql:host=localhost;dbname=csms',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
