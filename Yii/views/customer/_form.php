0d3b6e4f7ed9e167cc9b62a40cc65a85ba1d5ced
rain potoza <rypotoza2@gmal.com>
2017-09-06 09:16:58 +0800

    created customer/_form.php

---
+++ Yii/views/customer/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Customer */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="customer-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'cust_number')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'cust_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'cust_email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'cust_type')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
