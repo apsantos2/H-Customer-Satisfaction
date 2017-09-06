1a8c116674f49710093426b2459465c1dab9f8c6
rain potoza <rypotoza2@gmal.com>
2017-09-06 09:25:09 +0800

    update customer/_form.php

--- Yii/views/customer/_form.php
+++ Yii/views/customer/_form.php
@@ -21,7 +21,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'cust_type')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Delete', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
